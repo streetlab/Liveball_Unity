c4b72231b3d5132a35ac2959768193613f646b03
gunloves <gunloves@nate.com>
2015-08-10 11:21:26 +0900

    state changed to prefab

---
+++ Assets/Scripts/Common/Lobby/GameInfo/ScriptGameInfo.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptGameInfo : MonoBehaviour {
+
+	public GameObject mBtnMatchTop;
+	public GameObject mBtnPlayerTop;
+	public GameObject mBtnInfoTeam;
+	public GameObject mBtnRankTeam;
+	public GameObject mBtnInfoMatch;
+
+	enum TopState{
+		Match,
+		Player
+	}
+	TopState mTopState;
+
+	enum BtmState{
+		InfoTeam,
+		RankTeam,
+		InfoMatch
+	}
+	BtmState mBtmState;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MatchTopClicked(){
+		mTopState = TopState.Match;
+	}
+
+	public void PlayerTopClicked(){
+		mTopState = TopState.Player;
+	}
+
+	public void InfoTeamClicked(){
+		mBtmState = BtmState.InfoTeam;
+	}
+
+	public void RankTeamClicked(){
+		mBtmState = BtmState.RankTeam;
+	}
+
+	public void InfoMatchClicked(){
+		mBtmState = BtmState.InfoMatch;
+	}
+}
